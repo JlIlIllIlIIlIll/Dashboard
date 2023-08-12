8d857e25ce0517f9fad7b4ec51faa3e6af752f75
JlIlIllIlIIlIll <mohamed@ftouni.me>
2023-08-12 21:40:49 +0300

    Overview + Stripe

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import prismadb from "@/lib/prismadb";
+import { ModeToggle } from "@/components/theme-toggle";
 
 const Navabr = async () => {
   const { userId } = auth();
@@ -24,6 +25,7 @@ const Navabr = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ModeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
