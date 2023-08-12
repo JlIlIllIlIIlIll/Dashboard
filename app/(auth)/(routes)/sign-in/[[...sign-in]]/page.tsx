d7467f7adad7c2dcebeb93b17c840eb3a148c94b
JlIlIllIlIIlIll <mohamed@ftouni.me>
2023-08-12 23:53:50 +0300

    Final

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,31 @@
 import { SignIn } from "@clerk/nextjs";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Page() {
-  return <SignIn />;
+  return (
+    <>
+      <div className="pb-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Account Info</CardTitle>
+            <CardDescription>
+              Use the following details to sign in
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Email: test@test.com</p>
+            <p>Password: 123</p>
+          </CardContent>
+        </Card>
+      </div>
+      <SignIn />
+    </>
+  );
 }
